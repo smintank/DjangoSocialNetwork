e4b96efaa2ec818125fc269c008518481981a4b5
smintank <56520586+smintank@users.noreply.github.com>
2023-10-13 22:37:49 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = bool(int(os.getenv('DEBUG')))
+DEBUG = bool(int(os.getenv('DEBUG')), default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
